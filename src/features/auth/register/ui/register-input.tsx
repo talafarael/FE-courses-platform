d3735ead2e7d38b0a636c6629e1e5342b71952f4
qwertythome <qwrty.home@gmail.com>
2025-06-08 23:06:12 +0300

    register-update

--- src/features/auth/register/ui/register-input.tsx
+++ src/features/auth/register/ui/register-input.tsx
@@ -1,13 +1,19 @@
-import {Input} from "@/src/shared/ui/input/input";
+import { Input } from "@/src/shared/ui/input/input";
 
-export const RegisterInput=()=>{
-    return(
-        <div className={`flex flex-col gap-2`}>
-            <Input placeholder="Ім'я"/>
-            <Input placeholder="Прізвище"/>
-            <Input placeholder="Електронна пошта"/>
-            <Input placeholder="логін"/>
-            <Input placeholder="пароль" type="password"/>
+const fields = [
+    { placeholder: "Ім'я" },
+    { placeholder: "Прізвище" },
+    { placeholder: "Електронна пошта", type: "email" },
+    { placeholder: "Логін" },
+    { placeholder: "Пароль", type: "password" },
+];
+
+export const RegisterInput = () => {
+    return (
+        <div className="flex flex-col gap-2">
+            {fields.map(({ placeholder, type = "text" }) => (
+                <Input placeholder={placeholder} type={type} />
+            ))}
         </div>
-    )
-}
+    );
+};
