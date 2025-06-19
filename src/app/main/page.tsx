79e96df6097795cf8fc6f5f76763db5ce8abb8e8
talafarael <artemk2504@gmail.com>
2025-06-19 20:27:20 +0000

    feat:ok

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,33 +1,28 @@
-"use client"
+"use client";
 import { ProtectedRoute } from "@/src/shared/lib/router/protected-route";
-import '../style/globals.css'
+import "../style/globals.css";
 import React, { useEffect, useState } from "react";
-import  MainAuth  from "@/src/pages/main-page/authorized-main";
-import  MainUnAuth from "@/src/pages/main-page/unauthorized-main";
-import {getMeQuery} from "@/src/entities/user/api/user";
+import MainAuth from "@/src/pages/main-page/authorized-main";
+import MainUnAuth from "@/src/pages/main-page/unauthorized-main";
+import { getMeQuery } from "@/src/entities/user/api/user";
 
+export default function Home() {
+	const [isAuth, setIsAuth] = useState<boolean | null>(null);
 
-export default function Home () {
-    const [isAuth, setIsAuth] = useState<boolean | null>(null);
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		setIsAuth(!!token);
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        setIsAuth(!!token);
+		(async () => {
+			await getMeQuery();
+		})();
+	}, []);
 
+	if (isAuth === null) {
+		return <div>Загрузка...</div>;
+	}
 
-        (async  ()=> {
-            await getMeQuery()
-        })()
-
-    }, []);
-
-    if (isAuth === null) {
-        return <div>Загрузка...</div>;
-    }
-
-    return (
-      <ProtectedRoute >
-          {isAuth ? <MainAuth /> : <MainUnAuth />}
-      </ProtectedRoute>
-  );
+	return (
+		<ProtectedRoute>{isAuth ? <MainAuth /> : <MainUnAuth />}</ProtectedRoute>
+	);
 }
--- src/entities/user/api/user.ts
+++ src/entities/user/api/user.ts
@@ -1,17 +1,14 @@
-
 import { createApi } from "@/src/shared/api/axios";
-import {  AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 import { UserResponse } from "../type/user-api";
 
-export const getMeQuery = async (
+export const getMeQuery = async (): Promise<void> => {
+  const axios = createApi();
 
-): Promise<void> => {
-    const axios = createApi();
-
-    try {
-        const res: AxiosResponse<UserResponse> = await axios.get("/user/get-me");
-        console.log(res.data);
-    } catch (e) {
-        console.log(e+"hui")
-    }
+  try {
+    const res: AxiosResponse<UserResponse> = await axios.get("/user/get-me");
+    console.log(res.data);
+  } catch (e) {
+    console.log(e);
+  }
 };
