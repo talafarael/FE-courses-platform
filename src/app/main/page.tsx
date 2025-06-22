e4d9e62efe9fb43f48feb17580f719b06ad3789e
talaf <artemk2504@gmail.com>
2025-06-22 09:52:33 +0000

    hooks chagne

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -5,24 +5,18 @@ import React, { useEffect, useState } from "react";
 import MainAuth from "@/src/pages/main-page/authorized-main";
 import MainUnAuth from "@/src/pages/main-page/unauthorized-main";
 import { getMeQuery } from "@/src/entities/user/api/user";
+import { useUserStore } from "@/src/entities/user/model/userStore";
+import { useGetUser } from "@/src/entities/user/lib/useGetUser";
 
 export default function Home() {
-	const [isAuth, setIsAuth] = useState<boolean | null>(null);
+  const [isAuth, setIsAuth] = useState<boolean | null>(null);
+  const { } = useGetUser()
+  const { user } = useUserStore()
+  if (isAuth === null) {
+    return <div>Загрузка...</div>;
+  }
 
-	useEffect(() => {
-		const token = localStorage.getItem("token");
-		setIsAuth(!!token);
-
-		(async () => {
-			await getMeQuery();
-		})();
-	}, []);
-
-	if (isAuth === null) {
-		return <div>Загрузка...</div>;
-	}
-
-	return (
-		<ProtectedRoute>{isAuth ? <MainAuth /> : <MainUnAuth />}</ProtectedRoute>
-	);
+  return (
+    <ProtectedRoute>{isAuth ? <MainAuth /> : <MainUnAuth />}</ProtectedRoute>
+  );
 }
